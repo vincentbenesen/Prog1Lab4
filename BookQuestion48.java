3bc801e765e74c9eabc1fcb221740c2cd3f2c3df
vincentbenesen <60351647+vincentbenesen@users.noreply.github.com>
2020-03-31 16:44:18 -0400

    Add files via upload

---
+++ BookQuestion48.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4;
+
+/**
+ *
+ * @author vincentbenesen
+ */
+public class BookQuestion48 {
+    public static void main(String[] args) {
+
+        int i;
+        int multipleOfSeven = 0;
+        int total;
+
+        for (i = 33; i <= 97; i++) {
+            total = i % 7;
+
+            if (total == 0) {
+                multipleOfSeven++;
+            }
+        }
+        System.out.println("The number of multiples of seven" +
+                           " from 33 to 97 are " + multipleOfSeven);
+    }
+}
